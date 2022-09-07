60d79ffcafe0ecc43f5f86b59dd18c01f791a2d5
Austin Baird <austinzbaird@gmail.com>
2022-09-07 15:33:14 -0400

    svg

--- pages/secret.tsx
+++ pages/secret.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Secret() {
   return (
     <div>
-      <Image src="/BadNewsBairds.jpg" width="256px" height="256px" />
+      <Image src="/BadNewsBairds.svg" width="256px" height="256px" />
     </div>
   )
 }
