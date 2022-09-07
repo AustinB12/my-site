96e42bc25f84cebb242307bfa3b220666f8630f0
Austin Baird <austinzbaird@gmail.com>
2022-09-07 12:48:17 -0400

    BNB

---
+++ pages/secret.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Secret() {
+  return (
+    <div>
+      <Image src="/BadNewsBairds.png" width="256px" height="256px" />
+    </div>
+  )
+}
